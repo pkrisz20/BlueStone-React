d5ea074afda624f8a1d69b7b0ba742f5a27ec554
pkrisz20 <krisztianpasztor11@gmail.com>
2022-12-02 10:25:26 +0100

    Added sign up form validations

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,29 +1,43 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { userSchema } from "../validations/userValidation";
+import BlockTitle from './BlockTitle';
+import "../styles/components/Contact.scss";
 
 const ContactForm = () => {
 
-    const createUser = async (event) => {
-        event.preventDefault();
-        let formData = {
-            name: event.target[0].value,
-            email: event.target[1].value,
-            password: event.target[2].value,
-            confirmpassword: event.target[3].value,
-            age: event.target[4].value,
-        }
-        const isValid = await userSchema.isValid(formData);
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        mode: "onChange",
+        resolver: yupResolver(userSchema),
+    });
+
+    const submitForm = (data) => {
+        console.log(data);
     }
 
     return (
-        <form onSubmit={createUser}>
-            <input type="text" placeholder="Name..." />
-            <input type="email" placeholder="Email..." />
-            <input type="password" placeholder="Password..." />
-            <input type="password" placeholder="Confirm password..." />
-            <input type="number" placeholder="Age..." />
-            <input type="submit" />
-        </form>
+        <>
+            <BlockTitle title="Sign Up" />
+            <form className='form' onSubmit={handleSubmit(submitForm)}>
+                <input className={`form-input ${errors.fullName ? 'invalid' : ''}`} name="fullName" type="text" placeholder="Full name..." {...register("fullName")} />
+                {errors.fullName && <small className='form-error'>{errors.fullName?.message}</small>}
+
+                <input className={`form-input ${errors.email ? 'invalid' : ''}`} name="email" type="text" placeholder="Email..." {...register("email")} />
+                {errors.email && <small className='form-error'>{errors.email?.message}</small>}
+
+                <input className={`form-input ${errors.age ? 'invalid' : ''}`} name="age" type="number" placeholder="Age..." {...register("age")} />
+                {errors.age && <small className='form-error'>{errors.age?.message}</small>}
+
+                <input className={`form-input ${errors.password ? 'invalid' : ''}`} name="password" type="password" placeholder="Password..." {...register("password")} />
+                {errors.password && <small className='form-error'>{errors.password?.message}</small>}
+
+                <input className={`form-input ${errors.confirmpassword ? 'invalid' : ''}`} name="confirmpassword" type="password" placeholder="Confirm password..." {...register("confirmpassword")} />
+                {errors.confirmpassword && <small className='form-error'>{errors.confirmpassword?.message}</small>}
+
+                <input className={`form-submit ${Object.keys(errors).length !== 0 ? 'disabled' : ''}`} type="submit" value="Submit" />
+            </form>
+        </>
     );
 }
 
-export default ContactForm
+export default ContactForm;
--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -1,9 +1,11 @@
 import * as yup from 'yup';
 
 export const userSchema = yup.object().shape({
-    name: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup.string().min(4).max(10).required(),
-    confirmpassword: yup.string().oneOf[(yup.ref("password"), null)],
-    age: yup.number().positive().integer()
+    fullName: yup.string().required("Name is required"),
+    email: yup.string().email("Must be a valid email").required("Email is required"),
+    age: yup.number("Must be a number").positive("Must be greater than zero").integer("Must be an integer").transform(value => {
+        return Number.isNaN(value) ? 0 : value
+    }),
+    password: yup.string().min(4, "Password must be at least 4 characters").max(20, "Password must be at most 20 characters").required("Password is required"),
+    confirmpassword: yup.string().min(4, "Password must be at least 4 characters").max(20, "Password must be at most 20 characters").required("Password is required").oneOf([yup.ref("password"), null], "Passwords don't match")
 });
